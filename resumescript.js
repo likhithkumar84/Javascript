42cf143d3bffdcfcf14dd60038af63fbbc29e29c
Likhith Kumar <likhith.royal143@gmail.com>
2019-12-05 13:01:29 +0530

    javascript

---
+++ resumescript.js
@@ -0,0 +1,7 @@
+function background() {
+  var background = (document.body.style.backgroundColor = "burlywood");
+}
+function headingcolor() {
+  var headcolor = document.getElementById("heading_resume");
+  headcolor.style.color = "blue";
+}
