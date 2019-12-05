21854a98c75faf39d664cd0877a14ce5db1a7c94
Likhith Kumar <likhith.royal143@gmail.com>
2019-12-05 17:18:51 +0530

    change name,inner text, display date

--- resumescript.js
+++ resumescript.js
@@ -1,7 +1,22 @@
 function background() {
   var background = (document.body.style.backgroundColor = "burlywood");
 }
+var head = document.getElementById("heading_resume");
 function headingcolor() {
-  var headcolor = document.getElementById("heading_resume");
-  headcolor.style.color = "blue";
+  head.style.color = "blue";
+}
+function displaydate() {
+  var date = window.alert(Date());
+}
+function resumeheading() {
+  head.innerHTML = "Likhith's Resume";
+}
+function innertext() {
+  var textinside = document.getElementById("innertext");
+  textinside.innerText =
+    "A self-motivated IT professional with huge knowledge and proficiency in JavaScript, HTML , CSS, and mobile " +
+    "responsive web development, as well as strong skills and ability in writing clean and efficient code, seeks" +
+    " the " +
+    "position of Front-End Web Developer with XXX Tech.";
+  textinside.style.color = "crimson";
 }
