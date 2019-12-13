8f9f2940ba540d4657bd08c1ff32e166f7434e8e
Likhith Kumar <likhith.royal143@gmail.com>
2019-12-13 18:43:01 +0530

    todolist js

---
+++ ToDoListScript.js
@@ -0,0 +1,23 @@
+var pushdata = [];
+var inputlist = document.getElementById("list");
+var list = document.getElementById("innerlist");
+
+function insertdata() {
+    var inputlist = document.getElementById("list");
+  var todo = inputlist.value;
+  pushdata.push(todo);
+  list.innerHTML = "";
+  for (var i = 0; i < pushdata.length; i++) {
+    list.innerHTML += pushdata[i] + "<button data-index = '"+ i +"' onclick='remove(event)' >✖</button>" + "<br>";
+  }
+}
+
+function remove(event) {
+  pushdata.splice(event.target.dataset.index, 1);
+  console.log(pushdata);
+    list.innerHTML = "";
+    for (var i = 0; i < pushdata.length; i++) {
+        list.innerHTML += pushdata[i] + "<button data-index = '"+ i +"' onclick='remove(event)' >✖</button>" + "<br>";
+    }
+
+}
